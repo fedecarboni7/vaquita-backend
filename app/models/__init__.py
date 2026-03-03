ab5bfadc87bd6e27dbc9d995b72271e06fa7c4f7
fedecarboni7 <fedecarboni7@gmail.com>
2026-03-03 00:59:16 -0300

    Agregar modelo de usuario y establecer relación con transacciones

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.base import Base
 from app.models.transaction import Transaction, TransactionType
+from app.models.user import User
 
-__all__ = ["Base", "Transaction", "TransactionType"]
+__all__ = ["Base", "Transaction", "TransactionType", "User"]
