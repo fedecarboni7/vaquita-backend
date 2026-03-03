1afeb5440f050bc929c01f8e8840a6178b86c09b
fedecarboni7 <fedecarboni7@gmail.com>
2026-03-03 00:32:48 -0300

    Agregar configuración de Alembic para la actualización de la base de datos en el inicio de la aplicación

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
 
+from alembic.config import Config
+from alembic import command
 from fastapi import FastAPI
 from sqlalchemy import text
 
@@ -8,6 +10,8 @@ from app.database import engine
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
     yield
     await engine.dispose()
 
